fcc6a855373ff9ba2f304b68ba96717888c21a86
Shan John <shanjohn5308@gmail.com>
2023-10-22 21:49:41 +0530

    mojor UP

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,20 @@ import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:screw/constant/theme.dart';
 import 'package:screw/provider/app_provider.dart';
-
-
+import 'package:screw/screen/Responxive_layout_Error.dart';
+import 'package:screw/screen/responsive_Layout.dart';
 
 import 'package:screw/screen/welcome/splash_screen.dart';
 
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
- 
-  await Firebase.initializeApp();
 
- 
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyB_05BIX2I-tEEMQoKAbMJN64D84RFPMf4",
+          appId: "1:489111586757:android:452de786080201efb52dac",
+          messagingSenderId: "",
+          projectId: "screw-78f73"));
 
   SystemChrome.setPreferredOrientations(
     [
@@ -24,27 +26,20 @@ Future<void> main() async {
       DeviceOrientation.portraitUp,
     ],
   );
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
- class MyApp extends StatelessWidget {
-   MyApp({super.key});
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => AppProvider(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: themeData,
-        title: "Pas de dex",
-        home: 
-        
-SplashScreen()
-      )
-
-        
-     
-    );
+        create: (context) => AppProvider(),
+        child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: themeData,
+            title: "Pas de dex",
+            home: Responsive_Layout(MobielBody: SplashScreen(), DesktopBody: Responsive_Layout_Error_Screen())));
   }
 }
---
+++ lib/screen/responsive_Layout.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+class Responsive_Layout extends StatelessWidget {
+  final Widget MobielBody;
+  final Widget DesktopBody;
+  const Responsive_Layout({super.key, required this.MobielBody, required this.DesktopBody});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+         
+          return MobielBody;
+        } else {
+         
+          return DesktopBody;
+        }
+      },
+    );
+  }
+}
