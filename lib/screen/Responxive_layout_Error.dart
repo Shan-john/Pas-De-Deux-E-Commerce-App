b86917f27c33e2abf08ba610f637bcad154e05ac
Shan John <shanjohn5308@gmail.com>
2023-10-22 23:20:35 +0530

    up

--- lib/screen/Responxive_layout_Error.dart
+++ lib/screen/Responxive_layout_Error.dart
@@ -8,7 +8,7 @@ class Responsive_Layout_Error_Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body: SizedBox(
        
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width
